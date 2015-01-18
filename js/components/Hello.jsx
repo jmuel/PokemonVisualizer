f3fa4293391da7a314b9c840319a995fd7f70b42
jmuel <jmuel11@gmail.com>
2015-01-18 13:52:26 -0600

    Another small change to test heroku

--- js/components/Hello.jsx
+++ js/components/Hello.jsx
@@ -2,6 +2,6 @@ var React = require('react');
 
 module.exports = React.createClass({
     render: function () {
-        return (<p>Hello There!</p>);
+        return (<p>Hello There! Again!</p>);
     }
 });
