5a7a7b5a732957751036f7c98a587cbba2232967
jmuel <jmuel11@gmail.com>
2015-01-18 20:15:02 -0600

    We've got a chart

--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
 var React = require('react');
-var Hello = require('./components/hello.jsx');
+var Chart = require('./components/chart.jsx');
 
-React.render(<Hello/>, document.getElementById('root'));
+var sampleData = [
+    {id: '5fbmzmtc', x: 7, y: 41, z: 6},
+    {id: 's4f8phwm', x: 11, y: 45, z: 9},
+    {id: '532dfvma', x:13, y: 33, z: 12}
+];
+
+var App = React.createClass({
+    getInitialState: function() {
+        return {
+            data: sampleData,
+            domain: {x: [0, 30], y: [0, 100]}
+        };
+    },
+
+    render: function() {
+        return (
+            <div className="App">
+                <Chart
+                    data={this.state.data}
+                    domain={this.state.domain} />
+            </div>
+        );
+    }
+});
+
+React.render(<App/>, document.getElementById('root'));
