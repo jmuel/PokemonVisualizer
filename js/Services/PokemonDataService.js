60e5fd9a4cb7a0fa7ffb22cb1e593c7fb54531da
jmuel <jmuel11@gmail.com>
2015-02-07 18:32:38 -0600

    renamed a few things

---
+++ js/Services/PokemonDataService.js
@@ -0,0 +1,26 @@
+var d3 = require('d3')
+,   Marty = require('marty');
+
+var format = d3.time.format('%y-%b').parse;
+
+var data = {
+    Charizard: [
+        {x: format('14-JAN'), y: .40},
+        {x: format('14-FEB'), y: .30},
+        {x: format('14-MAR'), y: .10},
+        {x: format('14-APR'), y: .5},
+        {x: format('14-MAY'), y: .8},
+        {x: format('14-JUN'), y: .15},
+        {x: format('14-JUL'), y: .20}
+    ]
+};
+
+console.log(format('14-JAN'));
+
+var Service = Marty.createStateSource({
+    getUsage: function(pokemon) {
+        return data[pokemon];
+    }
+});
+
+module.exports = Service;
