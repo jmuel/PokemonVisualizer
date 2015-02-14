c1d70977ae774fa48b55e2f82b460a75409cc680
jmuel <jmuel11@gmail.com>
2015-02-13 22:00:40 -0600

    updated to use a better linux version

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,5 @@
 Vagrant.configure(2) do |config|
-  config.vm.box = "hashicorp/precise64"
+  config.vm.box = "ubuntu/trusty64"
 
   config.vm.network "forwarded_port", guest: 1337, host: 1338
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var compression = require('compression');
-var port = process.env.PORT || 12345;
+var port = process.env.PORT || 1337;
 
 
 app.get('/', function(req, res) {
